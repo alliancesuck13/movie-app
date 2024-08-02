9a6612ffcc3b4b228affca673e3280bb48f44769
Nikita Bautin <alliancesuck13@yandex.ru>
2024-08-02 20:26:17 +0700

    hotfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Online, Offline } from "react-detect-offline";
-import { Alert } from "antd";
 
 import "./index.css";
 import MovieApp from "./components/MovieApp";
@@ -9,16 +7,6 @@ import MovieApp from "./components/MovieApp";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Online>
-      <MovieApp />
-    </Online>
-    <Offline>
-      <Alert
-        banner="true"
-        message="No internet connection"
-        description="You are offline now =( Check your internet connection"
-        type="error"
-      />
-    </Offline>
+    <MovieApp />
   </React.StrictMode>
 );
